e22d398a36e0e280bf91e7dfde439cfcc63cc798
Ayonium <i@hyurl.com>
2019-10-28 00:47:16 +0800

    fix reply id bug

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ class SliMQ {
      * @param {(data: any) => void} reply
      */
     publish(topic, data, reply = null) {
-        let replyId = `${topic}$${this.serial.next().value}`;
+        let { clientId } = this.channel.options;
+        let replyId = `${topic}@${clientId}$${this.serial.next().value}`;
         topic = this.resolve(topic);
 
         if (reply) {
